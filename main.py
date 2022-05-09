8133028b0f0ee736f6ed0771cd8518a443060015
cdlaird2005 <ciaralaird5@gmail.com>
2022-05-08 21:14:36 -0500

    First version of Hangman

---
+++ main.py
@@ -0,0 +1,43 @@
+board = [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+]
+
+
+def print_board(board):
+    for row in board:
+        for slot in row:
+            print(f"{slot}", end="")
+        print()
+
+
+print_board(board)
+
+
+def quit(user_input):
+    if user_input.lower == "q":
+        print('Thanks for playing')
+        return True
+    else:
+        return False
+
+    def check_input(user_input):
+        isnum(user_input)
+
+
+def isnum(user_input):
+    if not user_input.isnumeric():
+        print("This is not a valid number")
+        return False
+    else:
+        return True
+
+
+while True:
+    print_board(board)
+    user_input = input("Please enter a position 1 through 9 or enter \"q\" to quit: ")
+    if quit(user_input): break
+    if not check_input(user_input):
+        print("Please try again.")
+        continue
